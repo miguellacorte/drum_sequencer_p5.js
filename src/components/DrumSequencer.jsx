f67c6ab7d477ed4357fffa46c7421f0fd1fc7f39
miguellacorte <76566384+miguellacorte@users.noreply.github.com>
2022-10-17 11:28:36 +0200

    bug wih p5.sound still needs to be fixed

--- src/components/DrumSequencer.jsx
+++ src/components/DrumSequencer.jsx
@@ -1,22 +1,22 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import Sketch from "react-p5";
 
-export default function DrumSequencer() {
+import "p5/lib/addons/p5.sound";
 
+export default function DrumSequencer() {
   let setup = (p5, canvasParentRef) => {
     let canvas = p5
       .createCanvas(p5.windowWidth, p5.windowHeight)
       .parent(canvasParentRef);
   };
 
-  let mousePressed = (p5) => {};
-
   let draw = (p5) => {};
 
-
-  return ( 
-  <div>
-  DrumSequencer
-  </div>
+  return (
+    <div>
+      <Sketch setup={setup} draw={draw}></Sketch>
+      DrumSequencer
+    </div>
   );
 }
